4ed5e1f51dd4997d5ca354c406dadbd6473d6065
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-07-14 21:29:36 +0700

    add data model

---
+++ web/src/app/models/status.model.ts
@@ -0,0 +1,7 @@
+export enum Status {
+  CONSIDERED,
+  PROCESSED,
+  DISCUSSED,
+  DOCUMENTED,
+  RESPONDED,
+}
--- web/src/app/services/tracking-service/tracking.service.ts
+++ web/src/app/services/tracking-service/tracking.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Status, TrackingData } from 'src/app/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrackingService {
-  trackingdata = {
-    track_id: 'CU123456TH',
-    topic: 'ร้องเรียนกรณีมีบุคคลภายนอกเข้ามาบริเวณหอพักนิสิต',
-    detail:
+  trackingdata: TrackingData = {
+    trackingId: 'CU123456TH',
+    title: 'ร้องเรียนกรณีมีบุคคลภายนอกเข้ามาบริเวณหอพักนิสิต',
+    details:
       'เมื่อวันที่ ... มีบุคคลน่าสงสัยเข้ามาภายในบริเวณหอพักนิสิต เวลา ...',
-    process: [
+    processes: [
       {
-        date: '23/04/2564 15:03',
-        detail:
+        timestamp: '23/04/2564 15:03',
+        details:
           'ได้รับการตอบกลับจากสำนักงานหอพักนิสิตแล้วผลสรุปว่าจะมีการจัดการปัญหาและเพิ่มมาตรการให้รัดกุมมากขึ้น',
         success: true,
       },
       {
-        date: '18/04/2564 15:03',
-        detail:
+        timestamp: '18/04/2564 15:03',
+        details:
           'ทางคณะกรรมาธิการวิสามัญสวัสดิการนิสิตได้ส่งหนังสือไปยังสำนักงานหอพักนิสิตเรียบร้อยแล้วและกำลังรอการตอบรับ',
         success: false,
       },
     ],
-    success_rate: 3,
+    status: Status.DOCUMENTED,
     owner: 'กินเก่ง น่ารัก',
   };
 
